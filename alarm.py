88bcd1932909ea721f51cfbced4470182f0d3238
prasanpro <prasanndutt@gmail.com>
2016-02-20 11:54:08 -0700

    Create alarm.py

---
+++ alarm.py
@@ -0,0 +1,9 @@
+import time
+import os
+
+
+print 'Hello waiting for 2 hours'
+time.sleep(7200)
+# need to install vlc first or else any other media player can be used
+os.system ("vlc my_song.mp4")
+
